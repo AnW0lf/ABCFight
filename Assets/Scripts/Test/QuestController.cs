0633778d36385abd070e0f4798713411da918a71
AnW0lf <AngryWolfgang@gmail.com>
2021-02-23 23:12:12 +0300

    window scaling

--- Assets/Scripts/Test/QuestController.cs
+++ Assets/Scripts/Test/QuestController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class QuestController : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _label = null;
     [SerializeField] private TMP_InputField _input = null;
     [SerializeField] private GameObject _window = null;
+    [SerializeField] private Image _fade = null;
 
     public UnityAction<string> OnSubmit { get; set; } = null;
 
@@ -19,6 +21,9 @@ public class QuestController : MonoBehaviour
         _questionData = questionData;
         _label.text = _questionData.Question;
         _window.SetActive(true);
+        StartCoroutine(Utils.CrossFading(Vector3.zero, Vector3.one, 0.4f, (scale) => _window.transform.localScale = scale, (a, b, c) => Vector3.Lerp(a, b, c)));
+        StartCoroutine(Utils.CrossFading(Vector3.forward * 126f, Vector3.zero, 0.4f, (angle) => _window.transform.localEulerAngles = angle, (a, b, c) => Vector3.Lerp(a, b, c)));
+        _fade.enabled = true;
         _input.text = "";
         _input.Select();
     }
@@ -34,6 +39,7 @@ public class QuestController : MonoBehaviour
         string value = ReadValue();
         if (string.IsNullOrEmpty(value) ||!_questionData.CheckAnswer(value)) return;
         _window.SetActive(false);
+        _fade.enabled = false;
         OnSubmit?.Invoke(value);
     }
 }
