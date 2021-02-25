e9e561e4cc082b784420e097ecf79d0356d16d73
SergeyVolkov <sergeyvolkovapplife@users.noreply.github.com>
2021-02-25 22:18:30 +0300

    changes

--- Assets/Scripts/Test/BotQuestController.cs
+++ Assets/Scripts/Test/BotQuestController.cs
@@ -7,12 +7,12 @@ public class BotQuestController : QuestController
 {
     public override void Begin(QuestionData questionData)
     {
-        StartCoroutine(Utils.DelayedCall(4f, () => Submit()));
+        StartCoroutine(Utils.DelayedCall(3.2f, () => Submit()));
     }
 
     public override void Submit()
     {
-        int count = Random.Range(3, 7);
+        int count = Random.Range(5, 5);
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < count; i++) sb.Append("a");
         OnSubmit?.Invoke(sb.ToString());
